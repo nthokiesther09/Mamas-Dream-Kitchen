08586bd9a9065a3eb62167f933b8bb33ec025222
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-20 21:27:39 +0300

    ft event listener for meal recipe

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 // event listeners
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
